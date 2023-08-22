39be28dc571db5392d5b5aed53da08ef353bbe28
Julie Hohenberg <juliehohenberg@gmail.com>
2023-08-22 16:22:51 -0400

    more tooltip work

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
             const tooltiptext = this.querySelector('.tooltiptext');
             if (tooltiptext) {
                 tooltiptext.style.opacity = '0';
-                tooltiptext.style.transition = 'opacity 1.0s ease-in-out';
+                tooltiptext.style.transition = 'opacity 1.2s ease-in-out';
                 setTimeout(() => {
                     tooltiptext.style.opacity = '1';
                 }, 50); // Small delay to trigger the transition
