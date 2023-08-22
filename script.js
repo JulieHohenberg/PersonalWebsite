6baa5adfb8ae9585d445f159adecefaff15f176b
Julie Hohenberg <juliehohenberg@gmail.com>
2023-08-22 16:03:09 -0400

    font and javascript update

---
+++ script.js
@@ -0,0 +1,23 @@
+// Tooltips animation
+    const tooltips = document.querySelectorAll('.tooltip');
+
+    tooltips.forEach(tooltip => {
+        tooltip.addEventListener('mouseover', function() {
+            const tooltiptext = this.querySelector('.tooltiptext');
+            if (tooltiptext) {
+                tooltiptext.style.opacity = '0';
+                tooltiptext.style.transition = 'opacity 1.0s ease-in-out';
+                setTimeout(() => {
+                    tooltiptext.style.opacity = '1';
+                }, 50); // Small delay to trigger the transition
+            }
+        });
+
+        tooltip.addEventListener('mouseout', function() {
+            const tooltiptext = this.querySelector('.tooltiptext');
+            if (tooltiptext) {
+                tooltiptext.style.opacity = '0';
+            }
+        });
+    });
+
